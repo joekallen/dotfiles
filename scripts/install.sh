7d7d20ed117d8832820e20d6d395282fe79dcf91
Joe Allen <joe.allen@subsplash.com>
2025-01-28 22:04:37 -0700

    .

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,6 @@
 
 set -eufo pipefail
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/joekallen/dotfiles/refs/heads/main/.chezmoitemplates/darwin-install-package-managers.sh)
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/joekallen/dotfiles/refs/heads/main/.chezmoitemplates/darwin-install-package-managers.sh)"
 
 chezmoi init --apply joekallen
